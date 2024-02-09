ee43873a6e721068710755eac514c660f3229288
Affaan Shaikh <147241008+affaan-shaikh@users.noreply.github.com>
2024-02-09 14:51:03 +0530

    Add files via upload

---
+++ modelConverter.py
@@ -0,0 +1,13 @@
+from keras.models import load_model
+import tensorflow as tf
+model= load_model('ALZ10Epochs.h5')
+
+converter= tf.lite.TFLiteConverter.from_keras_model(model)
+
+tf_lite_model= converter.convert()
+
+with open('model.tflite', 'wb') as f:
+    f.write(tf_lite_model)
+
+
+
